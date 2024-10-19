facb5dbc80b20d8612295c5ca7e9313264a26551
Alia Taher <alia.taher022@gmail.com>
2024-10-20 00:36:14 +0200

    assignment improved

--- Week1/assignment/meetup.js
+++ Week1/assignment/meetup.js
@@ -1,96 +1,70 @@
-import mysql2 from "mysql2";
+import mysql2 from "mysql2/promise";
 
-const connection = mysql2.createConnection({
-  host: "localhost",
-  user: "hyfuser",
-  password: "hyfpassword",
-  database: "meetup",
-});
+async function main() {
+  try {
+    const connection = await mysql2.createConnection({
+      host: "localhost",
+      user: "hyfuser",
+      password: "hyfpassword",
+      database: "meetup",
+    });
 
-connection.connect((error) => {
-  if (error) {
-    console.error("Error connecting: " + error.stack);
-    return;
-  }
-  console.log("Connected as id " + connection.threadId);
-});
+    await connection.connect(
+      console.log("Connected as id " + connection.threadId)
+    );
 
-const dropDatabase = `DROP DATABASE IF EXISTS meetup;`;
-const createDatabase = `CREATE DATABASE meetup;`;
+    const dropDatabase = `DROP DATABASE IF EXISTS meetup;`;
+    const createDatabase = `CREATE DATABASE meetup;`;
 
-connection.query(dropDatabase, (error) => {
-  if (error) throw error;
-  console.log("Existing database dropped.");
+    await connection.query(dropDatabase);
+    await connection.query(createDatabase);
+    await connection.query("USE meetup;");
+    console.log("Database created");
 
-  connection.query(createDatabase, (error) => {
-    if (error) throw error;
-    console.log("New database created.");
-
-    connection.query("USE meetup;", (error) => {
-      if (error) throw error;
-      console.log("Using the meetup database.");
-
-      const createTableInvitee = `
+    const createTableInvitee = `
   CREATE TABLE IF NOT EXISTS invitee (
-    invitee_num INT NOT NULL, 
+    invitee_num INT PRIMARY KEY AUTO_INCREMENT, 
     invitee_name VARCHAR(100) NOT NULL, 
-    invited_by VARCHAR(100),
-    PRIMARY KEY (invitee_num)
+    invited_by INT,
+    FOREIGN KEY (invited_by) REFERENCES invitee(invitee_num)
   )
  `;
 
-      const createTableRoom = `
+    const createTableRoom = `
  CREATE TABLE IF NOT EXISTS room (
- room_num INT NOT NULL,
+ room_num INT PRIMARY KEY,
  room_name VARCHAR(100),
- floor_number INT NOT NULL,
- PRIMARY KEY (room_num)
+ floor_number TINYINT NOT NULL
  )
  `;
 
-      const createTableMeeting = `
+    const createTableMeeting = `
  CREATE TABLE IF NOT EXISTS meeting (
- meeting_num INT NOT NULL,
+ meeting_num INT PRIMARY KEY,
  meeting_title VARCHAR(100) NOT NULL,
- starting_time TIME NOT NULL,
- ending_time TIME NOT NULL,
- room_num INT NOT NULL,
- PRIMARY KEY (meeting_num)
+ starting_time TIMESTAMP NOT NULL,
+ ending_time TIMESTAMP NOT NULL,
+ room_num INT NOT NULL
  )
  `;
 
-      connection.query(createTableInvitee, (error) => {
-        if (error) {
-          throw error;
-        }
-        console.log("Invitee table created");
-      });
+    await connection.query(createTableInvitee);
+    await connection.query(createTableRoom);
+    await connection.query(createTableMeeting);
+    console.log("Tables created");
+    
 
-      connection.query(createTableRoom, (error) => {
-        if (error) {
-          throw error;
-        }
-        console.log("Room Table created");
-      });
-
-      connection.query(createTableMeeting, (error) => {
-        if (error) {
-          throw error;
-        }
-        console.log("Meeting table created");
-      });
-
-      const insertInviteeData = `
-    INSERT IGNORE INTO invitee (invitee_num, invitee_name, invited_by) VALUES
-    (1, 'Alia','Sara'),(2, 'Seba', 'Sara'),(3, 'Salwa', 'Sara'),(4, 'Lamis', 'Sara'),(5, 'Buthina', 'Sara')
+    const insertInviteeData = `
+    INSERT IGNORE INTO invitee ( invitee_name, invited_by) VALUES
+    ('Alia', 1),('Seba', 1),('Salwa', 1),('Lamis', 1),('Buthina', 1)
     `;
 
-      const insertRoomData = `
+    const insertRoomData = `
     INSERT IGNORE INTO room (room_num, room_name, floor_number) VALUES
-  (879,'Single room', 8),(857,'Single room',8),(880,'Single room',8),(886,'Single room',8),(869,'Single room',8)
+     (879,'Single room', 8),(857,'Single room',8),(880,'Single room',8),(886,'Single room',8),(869,'Single room',8)
     `;
 
-      const insertMeetingData = `
+    const insertMeetingData = `
     INSERT IGNORE INTO meeting (meeting_num, meeting_title, starting_time, ending_time, room_num)  VALUES
     (1,'Kickoff Strategy','2024-12-15 10:00:00',' 2024-12-15 13:00:00',906),
     (2,'Milestone Review & Planning','2024-12-16 10:00:00',' 2024-12-16 15:00:00',432),
@@ -98,23 +72,24 @@ connection.query(dropDatabase, (error) => {
     (4,'Long-Term Strategic Planning','2024-12-18 10:00:00','2024-12-18 13:00:00',976),
     (5,'Creative Collaboration Session',' 2024-12-19 10:00:00','2024-12-19 16:00:00',906)
     `;
-      const insertQueries = [
-        insertInviteeData,
-        insertRoomData,
-        insertMeetingData,
-      ];
+    const insertQueries = [
+      insertInviteeData,
+      insertRoomData,
+      insertMeetingData,
+    ];
 
-      insertQueries.forEach((query, index) => {
-        connection.query(query, (error, results) => {
-          if (error) {
-            console.error(`Error executing query ${index + 1}:`, error.message);
-          } else {
-            console.log(`Query ${index + 1} executed successfully.`);
-          }
-        });
-      });
+    for (const [index, query] of insertQueries.entries()) {
+      try {
+        await connection.query(query);
+        console.log(`Query ${index + 1} executed successfully.`);
+      } catch (error) {
+        console.error(`Error executing query ${index + 1}:`, error.message);
+      }
+    }
 
-      connection.end();
-    });
-  });
-});
+    await connection.end();
+  } catch (error) {
+    console.error("Error :", error.message);
+  }
+}
+main();
