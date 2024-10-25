aeadf5d524273de5f0c4b9804fe031265e2524c8
azizsigar <aazizsigar@gmail.com>
2024-10-25 22:03:14 +0200

    non-repeated code implemented

--- Week1/worldDB.js
+++ Week1/worldDB.js
@@ -1,80 +1,37 @@
-import mysql from 'mysql2';
-//create connection to the database
+require("dotenv").config();
+const mysql = require("mysql2");
+
+// Create connection to the database using environment variables
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'hyfuser',
-    password: 'hyfpassword',
-    database: 'hello_worldismine'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-//What are the names of countries with population greater than 8 million?
-connection.query("SELECT name FROM country WHERE population > 8000000", function (error, results, fields) {
+// Function to execute a query and log the results
+const executeQuery = (query) => {
+  connection.query(query, (error, results, fields) => {
     if (error) {
-        throw error;
+      throw error;
     }
-    console.log("the reply is ", results);
-});
+    console.log("The reply is", results);
+  });
+};
 
-//What are the names of countries that have “land” in their names?
-connection.query("SELECT name FROM country WHERE name LIKE '%land%'", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What are the names of the cities with population in between 500,000 and 1 million?
-connection.query("SELECT name FROM city WHERE population BETWEEN 500000 AND 1000000", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What's the name of all the countries on the continent ‘Europe’?
-connection.query("SELECT name FROM country WHERE continent = 'Europe'", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//List all the countries in the descending order of their surface areas.
-connection.query("SELECT name FROM country ORDER BY surface_area DESC", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What are the names of all the cities in the Netherlands?
-connection.query("SELECT name FROM city WHERE country_code = 'NLD'", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What is the population of Rotterdam?
-connection.query("SELECT population FROM city WHERE name = 'Rotterdam'", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What's the top 10 countries by Surface Area?
-connection.query("SELECT name FROM country ORDER BY surface_area DESC LIMIT 10", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What's the top 10 most populated cities?
-connection.query("SELECT name FROM city ORDER BY population DESC LIMIT 10", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
-//What is the population number of the world?
-connection.query("SELECT SUM(population) FROM country", function (error, results, fields) {
-    if (error) {
-        throw error;
-    }
-    console.log("the reply is ", results);
-});
+// Queries to execute
+const queries = [
+  "SELECT name FROM country WHERE population > 8000000",
+  "SELECT name FROM country WHERE name LIKE '%land%'",
+  "SELECT name FROM city WHERE population BETWEEN 500000 AND 1000000",
+  "SELECT name FROM country WHERE continent = 'Europe'",
+  "SELECT name FROM country ORDER BY surface_area DESC",
+  "SELECT name FROM city WHERE country_code = 'NLD'",
+  "SELECT population FROM city WHERE name = 'Rotterdam'",
+  "SELECT name FROM country ORDER BY surface_area DESC LIMIT 10",
+  "SELECT name FROM city ORDER BY population DESC LIMIT 10",
+  "SELECT SUM(population) FROM country",
+];
+
+// Execute all queries
+queries.forEach(executeQuery);
