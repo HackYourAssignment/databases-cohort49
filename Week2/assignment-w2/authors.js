2b12052dd84f86dd90c87a259a3ee7e70d8b5240
bteicho@gmail.com <btegicho@gmail.com>
2024-11-01 11:27:26 +0100

    uodated based on the feedback

--- Week2/assignment-w2/authors.js
+++ Week2/assignment-w2/authors.js
@@ -1,23 +1,34 @@
 import connection from './dbconnection.js';
 
 const runQuery = (query, description) => {
-  connection.query(query, (err, result) => {
-    if (err) {
-      console.log(`Error executing ${description}:`, err);
-      return;
-    }
-    console.log(`${description} executed successfully`, result);
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.log(`Error executing ${description}:`, err);
+        reject(err);
+      } else {
+        console.log(`${description} executed successfully`, result);
+        resolve(result);
+      }
+    });
   });
 };
 // connection to the database
-connection.connect((err) => {
-  if (err) {
-    console.log('error connecting the database:', err);
-    return;
-  }
-  console.log('connected to the database');
+const setupDatabase = async () => {
+  try {
+    await new Promise((resolve, reject) => {
+      connection.connect((err) => {
+        if (err) {
+          console.log('error connecting the database:', err);
+          reject(err);
+        } else {
+          console.log('connected to the database');
+          resolve();
+        }
+      });
+    });
 
-  const createTableQuery = `
+    const createTableQuery = `
 CREATE TABLE IF NOT EXISTS authors(
    authors_id INT AUTO_INCREMENT PRIMARY KEY,
    author_name VARCHAR(100) NOT NULL, 
@@ -26,19 +37,23 @@ CREATE TABLE IF NOT EXISTS authors(
    h_index INT NOT NULL, 
    gender ENUM('m','f')
    );`;
-  runQuery(createTableQuery, 'creating the table');
 
-  const addMentorColumnQuery = `
+    const addMentorColumnQuery = `
 ALTER TABLE authors
 ADD COLUMN mentor INT;`;
-  runQuery(addMentorColumnQuery, 'adding mentor column');
 
-  const addForeignKeyQuery = `
+    const addForeignKeyQuery = `
 ALTER TABLE authors
 ADD CONSTRAINT fk_mentor 
 FOREIGN KEY (mentor) REFERENCES authors(authors_id) ON DELETE SET NULL;`;
+    await runQuery(createTableQuery, 'creating the table');
+    await runQuery(addMentorColumnQuery, 'adding mentor column');
+    await runQuery(addForeignKeyQuery, 'adding foreign key');
+  } catch (err) {
+    console.log('Error occurred while setting up the database:', err);
+  } finally {
+    connection.end();
+  }
+};
 
-  runQuery(addForeignKeyQuery, 'adding foreign key');
-
-  connection.end();
-});
+setupDatabase();
