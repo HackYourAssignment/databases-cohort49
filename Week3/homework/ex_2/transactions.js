4c6ad76a767d05cdee6724542fe2f74786ff6185
Muatasim-Aswad <malasoad2021@gmail.com>
2024-11-04 12:49:25 +0100

    change transfer function to be dynamic

--- Week3/homework/ex_2/transactions.js
+++ Week3/homework/ex_2/transactions.js
@@ -10,7 +10,7 @@ Here a hardcoded queries are used for simplicity
 and to show the transaction management.
 */
 
-async function main() {
+async function main(senderAcNu, receiverAcNu, amount) {
   //necessary for transactions. Otherwise a pool automatically establish connection
   const conn = await pool.getConnection();
 
@@ -22,12 +22,17 @@ async function main() {
     `);
     await conn.beginTransaction();
 
-    const [sender, receiver] = await getAccountsAndLockThem(conn);
-
+    const [sender, receiver] = await getAccountsAndLockThem(
+      conn,
+      senderAcNu,
+      receiverAcNu,
+    );
     isExistAndEnoughBalance(sender, receiver); //throws error if not
 
-    await Promise.all([performTransaction(conn), logTransaction(conn)]);
-
+    await Promise.all([
+      performTransaction(conn, senderAcNu, receiverAcNu, amount),
+      logTransaction(conn, senderAcNu, receiverAcNu, amount),
+    ]);
     await conn.commit(); //if everything is ok, commit the transaction
 
     console.log('Success: 1000 transferred from account 101 to account 102');
@@ -40,21 +45,24 @@ async function main() {
   }
 }
 
-main();
+main(101, 102, 1000.0);
 
-function getAccountsAndLockThem(conn) {
+function getAccountsAndLockThem(conn, senderAcNu, receiverAcNu) {
   const sender = `
     SELECT * FROM account
-    WHERE account_number = 101
+    WHERE account_number = ?
     FOR UPDATE;
   `;
-  const reciever = `
+  const receiver = `
     SELECT * FROM account
-    WHERE account_number = 102
+    WHERE account_number = ?
     FOR UPDATE;
   `;
 
-  return Promise.all([conn.query(sender), conn.query(reciever)]);
+  return Promise.all([
+    conn.execute(sender, [senderAcNu]),
+    conn.execute(receiver, [receiverAcNu]),
+  ]);
 }
 
 function isExistAndEnoughBalance(sender, receiver) {
@@ -65,30 +73,38 @@ function isExistAndEnoughBalance(sender, receiver) {
   return true;
 }
 
-function performTransaction(conn) {
+function performTransaction(conn, senderAcNu, receiverAcNu, amount) {
   const withdraw = `
     UPDATE account
-    SET balance = balance - 1000
-    WHERE account_number = 101;
+    SET balance = balance - ?
+    WHERE account_number = ?;
   `;
   const deposit = `
     UPDATE account
-    SET balance = balance + 1000
-    WHERE account_number = 102;
+    SET balance = balance + ?
+    WHERE account_number = ?;
   `;
 
-  return Promise.all([conn.query(withdraw), conn.query(deposit)]);
+  return Promise.all([
+    conn.execute(withdraw, [senderAcNu, amount]),
+    conn.execute(deposit, [receiverAcNu, amount]),
+  ]);
 }
 
-function logTransaction(conn) {
-  const logWithdraw = `
-    INSERT INTO account_changes (account_number, amount, changed_date, remark)
-    VALUES (101, -1000, CURDATE(), 'Transfer to account 102');
-  `;
-  const logDeposit = `
-    INSERT INTO account_changes (account_number, amount, changed_date, remark)
-    VALUES (102, 1000, CURDATE(), 'Transfer from account 101');
-  `;
+function logTransaction(conn, senderAcNu, receiverAcNu, amount) {
+  const logWithdraw = ` INSERT INTO account_changes (account_number, amount, changed_date, remark) VALUES (?, ?, CURDATE(), ?); `;
+  const logDeposit = ` INSERT INTO account_changes (account_number, amount, changed_date, remark) VALUES (?, ?, CURDATE(), ?); `;
 
-  return Promise.all([conn.query(logWithdraw), conn.query(logDeposit)]);
+  return Promise.all([
+    conn.query(logWithdraw, [
+      senderAcNu,
+      -amount,
+      `Transfer to account ${receiverAcNu}`,
+    ]),
+    conn.query(logDeposit, [
+      receiverAcNu,
+      amount,
+      `Transfer from account ${senderAcNu}`,
+    ]),
+  ]);
 }
